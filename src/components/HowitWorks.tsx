ca4b0c6aa93fa3424c12efc28578b2bf2fb920ec
Bhaveshanjana <bhaveshanjana58@gmail.com>
2025-08-15 17:13:47 +0530

    ui: update style

---
+++ src/components/HowitWorks.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { FlickeringGrid } from "./ui/bg";
+
+const HowitWorks = () => {
+  return (
+    <div>
+      <div className="relative mt-2 border-t-[1px] border-[#202020]">
+        <FlickeringGrid
+          className=" overflow-x-hidden inset-0 size-full "
+          squareSize={3}
+          gridGap={6}
+          color="#6c4ceb"
+          maxOpacity={0.2}
+          flickerChance={0.1}
+          height={180}
+          width={1540}
+        />
+        <div className="h-full  w-3xl bg-black blur-2xl rounded-full absolute flex  justify-center items-center inset-0 mx-auto"></div>
+
+        <h1 className="text-white text-4xl font-bold z-10 absolute inset-0 flex justify-center items-center">
+          How it Works
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default HowitWorks;

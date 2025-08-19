4efd46955c011bfa324abb6b0d64d67a1378404b
Bhaveshanjana <bhaveshanjana58@gmail.com>
2025-08-19 16:28:26 +0530

    add siteconfig

---
+++ src/data/siteconfig.ts
@@ -0,0 +1,21 @@
+// const baseUrl = "https://www.opemai.mom";
+
+export const siteConfig = {
+  authors: "Bhavesh",
+  name: "Open-ai",
+  description:
+    "Find your perfect Open-Source Repo. Discover top open-source projects in seconds. Filter by language, framework, or niche — and start contributing in seconds, not hours.",
+  //   url: baseUrl,
+  twitterHandle: "@Bhavesh2034",
+  keywords: [
+    "Open Source",
+    "Find GitHub Repos",
+    "Contribute to Open Source",
+    "Best Open Source Projects",
+    "Open Source Finder",
+    "Filter Repositories",
+    "Developer Contribution Platform",
+    "Open Source Community",
+    "GitHub Explorer",
+  ],
+};
